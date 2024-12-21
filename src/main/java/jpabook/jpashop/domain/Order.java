f59b29724b7fdc183d862995bef03fe27f29b8c5
KiHwanY <drg2524@naver.com>
2024-12-21 13:36:31 +0900

    Entity Mapping

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "member_id") // 양방향 연관관계 셋팅
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+    @OneToOne
+    private Delivery delivery;
+
+    private LocalDateTime orderDate; // 주문시간
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status; // 주문 상태 [ORDER , CANCEL] -> ENUM TYPE
+}
