b30b8dd92a2c7a62c2033b75ea2838e4384e33da
KiHwanY <drg2524@naver.com>
2025-01-05 16:48:33 +0900

    상품주문TDD 완료

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -1,6 +1,14 @@
 package jpabook.jpashop.service;
 
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.OrderRepository;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +23,8 @@ import static org.junit.Assert.*;
 @Transactional
 public class OrderServiceTest {
     @Autowired
+    EntityManager em;
+    @Autowired
     private OrderService orderService;
     @Autowired
     private OrderRepository orderRepository;
@@ -22,10 +32,27 @@ public class OrderServiceTest {
     @Test
     public void 상품주문 () throws Exception{
         //given
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울","강가","123-123"));
+        em.persist(member);
 
+        Book book= new Book();
+        book.setName("시골 JPA");
+        book.setPrice(10000);
+        book.setStockQuantity(10);
+        em.persist(book);
+        int orderCount = 2;
         //when
+        Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
 
         //then
+        Order getOrder = orderRepository.findOne(orderId);
+
+        Assert.assertEquals("상품 주문시 상태는 ORDER", OrderStatus.ORDER,getOrder.getStatus());
+        Assert.assertEquals("주문한 상품 종류 수가 정확해야 한다.", 1,getOrder.getOrderItems().size());
+        Assert.assertEquals("주문 가격은 가격 * 수량이다.", 10000 * orderCount,getOrder.getTotalPrice());
+        Assert.assertEquals("주문 수량만큼 재고가 줄어야 한다.", 8,book.getStockQuantity());
     }
 
     @Test
