54ab8bb33aecf32d470a17449cff0a86e469bd96
KiHwanY <drg2524@naver.com>
2025-01-16 22:52:55 +0900

    변경감지 테스트 코드 커밋

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,17 @@ public class ItemService {
     @Transactional // 우선권을 가진다.
     public void saveItem(Item item){
         itemRepository.save(item);
+    }
+
+    @Transactional //트랜잭션으로 인해 커밋이 된다.
+    public void updateItem(Long itemId, Book param){
+        //영속 상태
+        Item findItem = itemRepository.findOne(itemId);
+
+
+        findItem.setPrice(param.getPrice());
+        findItem.setName(param.getName());
+        findItem.setStockQuantity(findItem.getStockQuantity());
     }
 
     public List<Item> findItems(){
