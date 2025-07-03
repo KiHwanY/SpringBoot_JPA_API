9c07ff9d3ae16653a051bc00c55dfad5ad68b7c0
kihwanyun <drg2524@naver.com>
2025-07-03 09:22:40 +0900

    Member Create API Section

---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,53 @@
+package jpabook.jpashop.api;
+
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    //클래스를 별도로 생성안해도 된다.
+    //잘못하면 같이 망할 수 있는 로직이다.
+    // api 스펙이 바뀐다. -> entity 를 직접적으로 사용했기 때문에.
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    //member entity 가 바뀌어도 api에 전혀 영향이 받지 않는다.
+    //이 방식을 사용해야 한다.
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        private String name;
+
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,9 +14,13 @@ public class Member {
     @Id @GeneratedValue
     @Column(name = "member_id")
     private Long id;
+
+    @NotEmpty // null 보안
     private String name;
+
     @Embedded // 내장 타입
     private Address address;
+
     @OneToMany(mappedBy = "member") // 연관관계 주인 설정 -> FK
     private List<Order> orders = new ArrayList<>();
 
